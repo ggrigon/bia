b1b04eb9c78080a411f040c19e9d8d9f3798d3ba
Guilherme Rigon <guilherme@rigon.info>
2026-08-03 03:27:20 +0000

    alteracoes para CI CD

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Adicionar Tarefa ALB
+        Adicionar Task com CICD
       </button>
       
       <Modal
